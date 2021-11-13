486d56b7c369e23785de1a8bdf7d0ca339e5c8fc
onyshkevych-iryna <iryna.onyshkevych@valtech.com>
2021-11-13 16:42:57 +0200

    Try-catch added

--- TextApp/TextApp/Services/TextService.cs
+++ TextApp/TextApp/Services/TextService.cs
@@ -9,7 +9,6 @@ namespace TextApp.Services
 {
     public class TextService:ITextService
     {
-        public StreamReader sr;
         private string Path { get; set; }
         public TextService(string path)
         {
@@ -19,48 +18,71 @@ namespace TextApp.Services
         public Dictionary<string, int> WordsFrequency(string text)
         {
             Dictionary<string, int> collectionOfWords = new Dictionary<string, int>();
-            Regex exRegex = new Regex("[^a-zA-Z0-9']");
-            text = exRegex.Replace(text, " ");
-            string[] words = text.Split(new char[] {
-                ' '
-            }, StringSplitOptions.RemoveEmptyEntries);
-            var distinctWords = words.Select(i => i).OrderByDescending(i => i).Distinct();
-            string[] result = distinctWords.ToArray();
-            foreach (string word in result)
+            try
             {
-                FrequencyOfWord(text, word, collectionOfWords);
+                Regex exRegex = new Regex("[^a-zA-Z0-9']");
+                text = exRegex.Replace(text, " ");
+                string[] words = text.Split(new char[]
+                {
+                    ' '
+                }, StringSplitOptions.RemoveEmptyEntries);
+                var distinctWords = words.Select(i => i).OrderByDescending(i => i).Distinct();
+                string[] result = distinctWords.ToArray();
+                foreach (string word in result)
+                {
+                    FrequencyOfWord(text, word, collectionOfWords);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
             }
             return collectionOfWords;
         }
 
         public void FrequencyOfWord(string text, string word, Dictionary<string, int> dictionary)
         {
-            int count = 0;
-            int i = 0;
-            while ((i = text.IndexOf(word, i)) != -1)
+            try
             {
-                i += word.Length;
-                count++;
+                int count = 0;
+                int i = 0;
+                while ((i = text.IndexOf(word, i)) != -1)
+                {
+                    i += word.Length;
+                    count++;
+                }
+
+                dictionary.Add(word, count);
             }
-            dictionary.Add(word, count);
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
         }
 
         public void WordsPosition(string inputWord)
         {
-            sr = new StreamReader(Path);
-            string line = null;
-            List<string> listStrLineElements = null;
-            int lineNumber = 0;
-            while (!sr.EndOfStream)
+            try
             {
-                line = sr.ReadLine();
-                listStrLineElements = line.Split(' ').ToList();
-                var result1 = Enumerable.Range(0, listStrLineElements.Count)
-                    .Where(i => listStrLineElements[i] == inputWord)
-                    .ToList();
-                foreach (var item in result1)
-                    Console.WriteLine(String.Format($"line: {lineNumber+1} number of a word: {item + 1}"));
-                lineNumber++;
+                StreamReader sr = new StreamReader(Path);
+                string line = null;
+                List<string> listStrLineElements = null;
+                int lineNumber = 0;
+                while (!sr.EndOfStream)
+                {
+                    line = sr.ReadLine();
+                    listStrLineElements = line.Split(' ').ToList();
+                    var result = Enumerable.Range(0, listStrLineElements.Count)
+                        .Where(i => listStrLineElements[i] == inputWord)
+                        .ToList();
+                    foreach (var item in result)
+                        Console.WriteLine(String.Format($"line: {lineNumber + 1}  word number: {item + 1}"));
+                    lineNumber++;
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
             }
         }
     }
